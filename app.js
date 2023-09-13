3e3c9ca89c660076d3605a25f300950cc222aa01
Canaga81 <Canagamemmedov@gmail.com>
2023-09-13 17:53:38 +0400

    Populyar Html End

--- app.js
+++ app.js
@@ -55,3 +55,43 @@ const coursesActiveMain = document.querySelector('.courses-active');
 coursesBtn.addEventListener('click', () => {
     coursesActiveMain.classList.toggle('active');
 });
+
+
+//!------------------------ API -------------------------------------------------
+
+// const populyarLineContainer = document.querySelector('.populyar-line-container');
+
+// const PostUI = (posts) => {
+
+//     let result = '';
+
+//     posts.forEach((post) => {
+
+//         result += `
+//         <li class="post-item">
+//                 <div class="post-body">
+//                     <span class="post-id">Post Id: <b>${post.id}</b></span>
+//                     <strong class="post-title">${post.title}</strong>
+//                     <p class="post-content">${post.body}</p>
+//                 </div>
+
+//                 <button class="post-button">Read More</button>
+//             </li>
+//         `;
+
+//         postList.innerHTML = result;
+
+//     });
+
+// }
+
+// const getData  = async () => {
+
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     const data = await res.json();
+
+//     PostUI(data);
+
+// }
+
+// window.addEventListener("DOMContentLoaded", getData);
