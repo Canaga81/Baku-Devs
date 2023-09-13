4a372f35d63e09eb09955711ba77f752be0729f1
Canaga81 <Canagamemmedov@gmail.com>
2023-09-13 16:24:33 +0400

    Container-Active

---
+++ app.js
@@ -0,0 +1,45 @@
+const xMarkButton = document.querySelector('.icon-mark');
+const barsButton = document.querySelector(".icon-list");
+
+const picture = document.querySelector(".container");
+const pictureActive = document.querySelector(".container-active");
+
+barsButton.addEventListener("click", () => {
+    pictureActive.style.display = "block";
+    picture.style.display = "none";
+})
+
+xMarkButton.addEventListener("click", () => {
+    pictureActive.style.display = "none";
+    picture.style.display = "block";
+});
+
+
+
+//! ----------------------------------------------------
+
+const frontEnd = document.getElementById('frontend');
+const backEnd = document.getElementById('backend');
+const uIUx = document.getElementById('uiux');
+
+const containerActiveRight = document.querySelector('.container-active-right');
+const containerActiveCent = document.querySelector('.container-active-cent');
+const containerActiveEnd = document.querySelector('.container-active-end');
+
+frontEnd.addEventListener('click', () => {
+    containerActiveRight.classList.add('front');
+    containerActiveCent.classList.remove('back');
+    containerActiveEnd.classList.remove('ui');
+});
+
+backEnd.addEventListener('click', () => {
+    containerActiveCent.classList.add('back');
+    containerActiveRight.classList.remove('front');
+    containerActiveEnd.classList.remove('ui');
+});
+
+uIUx.addEventListener('click', () => {
+    containerActiveEnd.classList.add('ui');
+    containerActiveRight.classList.remove('front');
+    containerActiveCent.classList.remove('back');
+});
