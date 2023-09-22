ad3775115f97a8d369311b4a8d00c2a38c87c814
Canaga81 <Canagamemmedov@gmail.com>
2023-09-22 20:56:02 +0400

    Write Js

---
+++ write.js
@@ -0,0 +1,24 @@
+const knowledgePEl = document.getElementById( 'knowledgeP' );
+const speedEl = document.getElementById( 'speed' );
+
+const text = `Nə üçün tədrisinizi məhz bizdə keçməlisiniz? Bunun üçün ən azı dörd səbəb var:`;
+let idx = 1;
+let speed = 200 / speedEl.value;
+
+writeText();
+
+function writeText () {
+
+    knowledgePEl.innerText = text.slice( 0, idx );
+    idx++;
+
+    if ( idx > text.length ) {
+
+        //*** content.innerText => (Dongunu Dayandirir);
+        idx = 1; //! => (Dongunu Yaradir)
+        
+    }
+
+    setTimeout( writeText, speed );
+
+}
