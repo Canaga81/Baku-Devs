e5c893678a8ac128ba2fe4eb2a0709e3a130bdf4
Canaga81 <Canagamemmedov@gmail.com>
2023-09-28 15:10:17 +0400

    Kabinet Html Css Js The End

--- form.js
+++ form.js
@@ -117,6 +117,7 @@ xMark2Btn.addEventListener( 'click', () => {
     kabinetPage2.classList.remove( 'active2' );
 } );
 
+//! Api
 
 const populyarLineContainer = document.querySelector( '.populyar-line-container' );
 
@@ -219,3 +220,106 @@ const getData = async () => {
 };
 
 window.addEventListener( "DOMContentLoaded", getData );
+
+//! Tiklanan'da active olan sehifeler
+
+//? Pages
+
+const kabinetEmail = document.querySelector( '.kabinet-email' );
+const kabinetAbune = document.querySelector( '.kabinet-abune' );
+const kabinetQebz = document.querySelector( '.kabinet-qebz' );
+const populyarLineContainerEl = document.querySelector( '.populyar-line-container' );
+
+//? Buttons
+
+const profileBtn = document.getElementById( 'profile' );
+const abunelikBtn = document.getElementById( 'abunelik' );
+const qebzlerBtn = document.getElementById( 'qebzler' );
+const istekBtn = document.getElementById( 'istek' );
+const cixisBtn = document.getElementById( 'cixis' );
+
+profileBtn.addEventListener( 'click', () => {
+    kabinetEmail.classList.add( 'active' );
+    kabinetAbune.classList.remove( 'active' );
+    kabinetQebz.classList.remove( 'active' );
+    populyarLineContainerEl.classList.remove('active')
+} );
+
+abunelikBtn.addEventListener( 'click', () => {
+    kabinetAbune.classList.add( 'active' );
+    kabinetEmail.classList.remove( 'active' );
+    kabinetQebz.classList.remove( 'active' );
+    populyarLineContainerEl.classList.remove('active')
+} );
+
+qebzlerBtn.addEventListener( 'click', () => {
+    kabinetQebz.classList.add( 'active' );
+    kabinetEmail.classList.remove( 'active' );
+    kabinetAbune.classList.remove( 'active' );
+    populyarLineContainerEl.classList.remove('active')
+} );
+
+istekBtn.addEventListener( 'click', () => {
+    populyarLineContainerEl.classList.add( 'active' );
+    kabinetQebz.classList.remove( 'active' );
+    kabinetEmail.classList.remove( 'active' );
+    kabinetAbune.classList.remove( 'active' );
+} );
+
+//! ---------------------- Active Card ------------------------------------------
+
+const xMarkButton = document.querySelector( '.icon-mark' );
+const barsButton = document.querySelector( ".icon-list" );
+
+const picture = document.querySelector( ".container" );
+const pictureActive = document.querySelector( ".container-active" );
+
+barsButton.addEventListener( "click", () => {
+    pictureActive.style.display = "block";
+    picture.style.display = "none";
+} );
+
+xMarkButton.addEventListener( "click", () => {
+    pictureActive.style.display = "none";
+    picture.style.display = "block";
+} );
+
+
+//! ---------------------- Active A ---------------------------------------------
+
+const frontEnd = document.getElementById( 'frontend' );
+const backEnd = document.getElementById( 'backend' );
+const uIUx = document.getElementById( 'uiux' );
+
+const containerActiveRight = document.querySelector( '.container-active-right' );
+const containerActiveCent = document.querySelector( '.container-active-cent' );
+const containerActiveEnd = document.querySelector( '.container-active-end' );
+
+frontEnd.addEventListener( 'click', () => {
+    containerActiveRight.classList.add( 'front' );
+    containerActiveCent.classList.remove( 'back' );
+    containerActiveEnd.classList.remove( 'ui' );
+} );
+
+backEnd.addEventListener( 'click', () => {
+    containerActiveCent.classList.add( 'back' );
+    containerActiveRight.classList.remove( 'front' );
+    containerActiveEnd.classList.remove( 'ui' );
+} );
+
+uIUx.addEventListener( 'click', () => {
+    containerActiveEnd.classList.add( 'ui' );
+    containerActiveRight.classList.remove( 'front' );
+    containerActiveCent.classList.remove( 'back' );
+} );
+
+
+//! ---------------------- Active Courses ---------------------------------------
+
+const coursesBtn = document.getElementById( 'courses' );
+const coursesActiveMain = document.querySelector( '.courses-active' );
+
+
+coursesBtn.addEventListener( 'click', () => {
+    coursesActiveMain.classList.toggle( 'active' );
+} );
