ecacdfdd15fed4b916283b6b376de9bcc040c5b8
Canaga81 <Canagamemmedov@gmail.com>
2023-10-02 20:50:08 +0400

    Elaqe Form-Control Js

--- form_2.js
+++ form_2.js
@@ -7,3 +7,42 @@ labels.forEach( ( label ) => {
         .map( ( letter, index ) => `<span style="transition-delay: ${ index * 50 }ms;">${ letter }</span>` )
         .join( '' );
 } );
+
+//! ---------- Form Control ----------
+
+const inputText = document.getElementById( 'input-text' );
+const inputEmail = document.getElementById( 'input-email' );
+const inputNumber = document.getElementById( 'input-number' );
+const formTextarea = document.getElementById( 'form-textarea' );
+const questionBtn = document.getElementById( 'question-btn' );
+const form = document.getElementById( 'form' );
+
+form.addEventListener( 'submit', e => {
+    e.preventDefault();
+} );
+
+inputNumber.addEventListener( 'input', e => {
+
+    let inputValue = e.target.value.replace( /\D/g, '' ); //! Sadece Reqemler Al
+    let formattedValue = '';
+
+    for ( let i = 0; i < inputValue.length; i++ ) {
+
+        formattedValue += inputValue[i];
+
+    }
+
+    e.target.value = formattedValue;
+
+} );
+
+inputText.addEventListener( 'input', e => {
+
+    let inputValue = e.target.value;
+    let lettersOnly = inputValue.replace( /[^a-zA-Z]/g, '' );
+
+    if ( inputValue !== lettersOnly ) {
+        e.target.value = lettersOnly; //! Sadece harf karakterlerini geri yazar.
+    }
+
+} );
